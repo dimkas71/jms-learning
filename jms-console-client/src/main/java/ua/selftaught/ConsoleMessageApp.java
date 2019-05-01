db586cf75c491bd51a4e3788c2f4870c8292bbe9
dimkas71g@gmail.com <dimkas71g@gmail.com>
2019-05-01 10:35:00 +0300

    Add producer in console app to send message

--- jms-console-client/src/main/java/ua/selftaught/ConsoleMessageApp.java
+++ jms-console-client/src/main/java/ua/selftaught/ConsoleMessageApp.java
@@ -1,9 +1,39 @@
 package ua.selftaught;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
+import javax.jms.JMSException;
+
 public class ConsoleMessageApp {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	private static final String QUEUE = "main queue";
+
+	public static void main(String[] args) throws JMSException, UnsupportedEncodingException {
+		
+
+		Producer p = new Producer();
+		p.create(QUEUE);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter new message(to exit use 'Y')>");
+		
+		while (sc.hasNext()) {
+			String toSend = sc.next();
+			
+			if (toSend.equalsIgnoreCase("Y")) {
+				break;
+			}
+			
+			p.sendMessage(toSend);
+			
+		}
+		
+		sc.close();
+		
+		p.close();
 
 	}
 
